ee87cbd54dc1852f1b9bc05d111270757f339115
devcon13 <devon_connerly24@milton.edu>
2023-05-10 21:19:31 -0400

    I couldn't open the read file given for some reason, so I tried to get it from the internet but I still don't really understand how to do it and I couldn't finish.

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,10 @@ import java.net.URL;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 
@@ -17,11 +21,39 @@ public class Main {
     public JTextArea stats;
     public JTextArea pokemon;
 
-    public static void main(String[] args) {
-        new Main();
-    }
+    public static void main(String[] args) {new Main();}
     public Main(){
         prepareGUI();
+        String pokemonName = "pikachu"; // Change this to the desired Pokémon name
+
+        try {
+            // Make a GET request to the PokeAPI
+            URL url = new URL("https://pokeapi.co/api/v2/pokemon/" + pokemonName);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            // Read the response
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder response = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+            reader.close();
+
+            // Parse the JSON response
+            // Here, you can extract the name and stats of the Pokémon
+            // For simplicity, let's assume you only want the base stats
+            String name = response.toString().split("\"name\":\"pikachu\"")[1].split("\"")[0];
+            String stats = response.toString().split("\"stats\":")[1].split("],")[0];
+
+            // Print the name and stats
+            System.out.println("Name: " + name);
+            System.out.println("Stats: " + stats);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
